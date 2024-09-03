bc35be73c561bbcbcf52763d73514659e7a2bb61
justinpope99 <115605273+justinpope99@users.noreply.github.com>
2024-09-03 19:43:23 -0400

    Update axiosConfig.js to include the new API URL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,10 +1,10 @@
 // This file will be used to configure axios
 // We can configure and export the Axios object that we will use to make HTTP Requests to the API
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    // The baseURL provides the base address of the API endpoints that our app will be calling
-    baseURL:'https://expensive-ellissa-justin-pope-f21c5acc.koyeb.app',
-    timeout: 20000,
-    headers: {"Content-type": "application/json"}
-})
+  // The baseURL provides the base address of the API endpoints that our app will be calling
+  baseURL: "https://golden-mignonne-justin-pope-beb53e33.koyeb.app",
+  timeout: 20000,
+  headers: { "Content-type": "application/json" },
+});
