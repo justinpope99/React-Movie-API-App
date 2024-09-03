140328c79951aaa6277bbeb5d9a5ad57f2c44184
justinpope99 <115605273+justinpope99@users.noreply.github.com>
2024-09-03 19:59:26 -0400

    Update Header to include a link to the GitHub repository and Search page, and remove the Watch List link. Also add a ToastContainer to display Toastify notifications. The Login and Register buttons will now display a notification letting the user know that they have not been implemented yet.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,31 +4,72 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Header = () => {
+  const login = () => {
+    toast.info(
+      "The Login Feature has not been implemented yet. Please check back later.",
+      {
+        theme: "light",
+        position: "top-right",
+      }
+    );
+  };
+
+  const register = () => {
+    toast.info(
+      "The Register Feature has not been implemented yet. Please check back later.",
+      {
+        theme: "light",
+        position: "top-right",
+      }
+    );
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-            <Navbar.Brand href="/" style={{"color": 'gold'}}>
-                <FontAwesomeIcon icon={faVideoSlash}/>Gold
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-                <Nav
-                    className="me-auto my-2 my-lg-0"
-                    style={{maxHeight: '100px'}}
-                    navbarScroll
-                >
-                    <NavLink className="nav-link" to="/">Home</NavLink>
-                    <NavLink className="nav-link" to="/watchList">Watch List</NavLink>
-                </Nav>
-                <Button variant="outline-info" className="me-2">Login</Button>
-                <Button variant="outline-info">Register</Button>
-            </Navbar.Collapse>
-        </Container>
+      <Container fluid>
+        <Navbar.Brand href="/" style={{ color: "gold" }}>
+          <FontAwesomeIcon icon={faVideoSlash} />
+          Gold
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            {/* <NavLink className="nav-link" to="/watchList">
+              Watch List
+            </NavLink> */}
+            <NavLink className="nav-link" to="/Search">
+              Search
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              to="https://github.com/justinpope99/React-Movie-API-App"
+            >
+              © 2024 Justin Pope
+            </NavLink>
+          </Nav>
+          <Button variant="outline-info" className="me-2" onClick={login}>
+            Login
+          </Button>
+          <Button variant="outline-info" onClick={register}>
+            Register
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+      <ToastContainer />
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
