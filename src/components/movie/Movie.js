2451b23a089bd8fd5c72fd7d5d8cd3974b513bed
justinpope99 <115605273+justinpope99@users.noreply.github.com>
2024-09-03 21:22:47 -0400

    Add a button on the Movie page that allows the user to go to the Trailer page

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,7 +1,9 @@
 import "./Movie.css";
 import { Button } from "react-bootstrap";
 import { useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Movie = ({ movie, getMovieData }) => {
   const navigate = useNavigate();
@@ -23,9 +25,19 @@ const Movie = ({ movie, getMovieData }) => {
         <h4>{`Directed by ${movie?.director}`}</h4>
         <h4>{`Released ${movie?.releaseDate}`}</h4>
         <img src={movie?.poster} alt={`Poster of ${movie?.title}`} />
-        <Button variant="outline-info" onClick={() => reviews(movie?.imdbId)}>
-          Go to Reviews
-        </Button>
+        <div className="movie-page-buttons-container">
+          <Button variant="outline-info" onClick={() => reviews(movie?.imdbId)}>
+            Go to Reviews
+          </Button>
+          <Link
+            to={`/Trailer/${movie?.trailerLink.substring(
+              movie.trailerLink.length - 11
+            )}`}
+          >
+            <Button variant="outline-info">Go to Trailer</Button>
+          </Link>
+        </div>
+
         <h4>Overview</h4>
         <p>{movie?.overview}</p>
       </div>
